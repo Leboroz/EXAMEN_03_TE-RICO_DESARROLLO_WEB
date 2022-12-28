aa8d8553ca4ef0f571ae3ca135a3e93a9cc5efd6
Leonardo Albornoz <albornozleo95@gmail.com>
2022-12-28 16:25:10 -0400

    Create model

---
+++ server/src/main/java/com/examenteorico/examen/model/Venta.java
@@ -0,0 +1,23 @@
+package com.examenteorico.examen.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@Table(name = "Venta")
+public class Venta {
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Long id;
+  private String description;
+  private Integer quantitySold;
+  private Float unitaryPrice;
+  private double total;
+}
